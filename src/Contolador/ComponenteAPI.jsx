326a4a3434d79862d8da95b042c1c4a4671094f9
julieta.noguera.anmat@outlook.com <julieta.noguera.anmat@outlook.com>
2023-11-22 15:32:43 -0300

    arreglos de la api

--- src/Contolador/ComponenteAPI.jsx
+++ src/Contolador/ComponenteAPI.jsx
@@ -1,53 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Card,  Col,  Container, Row } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button } from 'react-bootstrap';
 
 const ComponenteAPI = () => {
-    const [flowers, setFlowers] = useState([]);
+  const [flowerPhotos, setFlowerPhotos] = useState([]);
 
-    useEffect(() => {
-      const apiUrl = 'https://api.unsplash.com/photos/?query=flowers&orientation=portrait&color=red&client_id=AmlGWTJ5RK4Yo13n07JueVkC8SD7AHhj6YWvstXvhgg&count=12';
-  
-      fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => setFlowers(data))
-        .catch(error => console.error('Error fetching flowers:', error));
+  useEffect(() => {
+    const apiUrl = 'https://api.unsplash.com/search/photos?page=1&query=flores-rojas&client_id=AmlGWTJ5RK4Yo13n07JueVkC8SD7AHhj6YWvstXvhgg&per_page=12';
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => setFlowerPhotos(data.results))
+      .catch(error => console.error('Error fetching flower photos:', error));
     }, []);
-  
-    return (
-        <div className='presentacion'>
+
+    const handleDownloadClick = (photoUrl, photoId) => {
+        const link = document.createElement('a');
+        link.href = photoUrl;
+        link.download = `flor_${photoId}.jpg`; // Puedes personalizar el nombre del archivo aquí
+        link.click();
+      };
+
+  return (
+    <div className='presentacion'>
             <h3>Galeria de imagenes</h3>
             <p>Explora nuestra galería de imágenes, sumérgete en la frescura de los pétalos y 
                 déjate llevar por la armonía de los colores. Estas fotos no solo son un vistazo a nuestra colección,
                  sino también una invitación a experimentar la calidad y el encanto que Flor de Lis 
                  tiene para ofrecer. 
-                 <br/>¡Esperamos que encuentres la inspiración que buscas para dar vida a 
-                 tus momentos especiales con nuestras hermosas flores!</p>
-
-    
-         <Container className="container">
-            <Row className="carruRow">
-                {flowers.map(flower => (
-                <Col key={flower.id} xs={12} md={3} lg={4}>
+                 <br></br>
+                 <b>¡Esperamos que encuentres la inspiración que buscas con las imagenes que nos pasaron nuestros
+                    clientes!</b></p>
+            <Container className="container">
+                <Row className="carruRow">
+                {flowerPhotos.map(photo => (
+                    <Col key={photo.id} xs={12} md={3} lg={4}>
                     <Card className="card-margin">
-                    <Card.Img
+                        <Card.Img
                         variant="top"
-                        src={flower.urls.small}
-                        alt={flower.alt_description}
+                        src={photo.urls.small}
+                        alt={photo.alt_description}
                         className="img-fluid carruImg"
-                    />
-                    <Card.Body className="carruBody">
-                        <Card.Title>{flower.alt_description || 'Sin descripción'}</Card.Title>
-                        {/* Puedes agregar más detalles aquí según tus necesidades */}
-                        <Button className="botonPri" variant="primary" size="sm" >Ver más</Button>
-                    </Card.Body>
+                        />
+                        <Card.Body className="carruBody">
+                        <Card.Title>{photo.description || 'Sin descripción'}</Card.Title>
+                        
+                        <Button
+                            className="botonPri"
+                            variant="primary"
+                            size="sm"
+                            onClick={() => handleDownloadClick(photo.urls.full, photo.id)}
+                        >
+                            Ver
+                        </Button>
+                        </Card.Body>
                     </Card>
-                </Col>
+                    </Col>
                 ))}
-            </Row>
+                </Row>
             </Container>
-      </div>
-
-    );
+    </div>
+  );
 };
 
 export default ComponenteAPI;
